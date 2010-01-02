e23862af0ca9e42519704c5e893898e8304a3a8c
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-02 17:45:35 +0800

    correction des bugs d'affichages du mail

--- modules/members/signup.php
+++ modules/members/signup.php
@@ -64,12 +64,14 @@
 				
 				
 				// Preparation du mail
-				$message_mail = "<html><head></head><body> <p>Merci de vous être inscrit sur '\mon site\' !</p> <p>Veuillez cliquer sur <a href=\"http://".$_SERVER['PHP_SELF']."?module=members&amp;action=valid_account&amp;hash=\".$hash_validation."\">ce lien</a> pour activer votre compte !</p></body></html>";
+				$message_mail = "<html><head></head><body> <p>Merci de vous être inscrit sur <b>Nabzfood</b> !</p>
+				<p>Veuillez cliquer sur <a href=\"http://".$_SERVER['PHP_SELF']."?module=members&amp;action=valid_account&amp;hash=".$hash_validation."\">ce lien</a> pour activer votre compte !</p></body></html>";
 				
 				$headers_mail  = 'MIME-Version: 1.0'                           ."\r\n";
 				$headers_mail .= 'Content-type: text/html; charset=utf-8'      ."\r\n";
 				$headers_mail .= 'From: "Mon site" <contact@monsite.com>'      ."\r\n";
 				
+				$email_addr = $form_signup->get_cleaned_data('email_addr');
 				// Envoi du mail
 				include './modules/mail/mail.php';
 				//mail($form_signup->get_cleaned_data('email_addr'), 'signup sur <monsite.com>', $message_mail, $headers_mail);
