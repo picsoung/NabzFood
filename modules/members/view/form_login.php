152bda0b4ddded0bb88ce649f7ca5edf0557becc
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-02 10:57:48 +0800

    Ajout d'une ligne pour indiquer comment reseter son pwd

--- modules/members/view/form_login.php
+++ modules/members/view/form_login.php
@@ -2,6 +2,7 @@
 
 <p>Si vous n'êtes pas encore inscrit, vous pouvez le faire en <a href="index.php?module=members&amp;action=signup">cliquant sur ce lien</a>.</p>
 
+<p>Si vous avez oublié votre mot de passe vous pouvez utiliser le formulaire de renvoi pour le retrouver : <a href="index.php?module=members&amp;action=reset_pwd"> formulaire de renvoi de mot de passe </a> </p>
 <?php
 
 if (!empty($errors_login)) {
