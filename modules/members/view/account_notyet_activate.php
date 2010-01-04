d24d66f18feb7a54042b36e8a6d43a3d67bda012
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-04 21:35:08 +0100

    À afficher si un compte n'et pas encore activé

---
+++ modules/members/view/account_notyet_activate.php
@@ -0,0 +1,5 @@
+<h2>Compte non activé </h2>
+
+<p>Votre compte n'est pas encore activé vous ne pouvez vous connecter à l'interface de Nabzfood.</p>
+<p>Pour activer votre compte vous devez cliquer sur le lien se trouvant dans le mél envoyé lors de votre inscription</p>
+<p>Nous pouvons aussi vous renvoyer ce lien : <a href="index.php?module=members&amp;action=reset_pwd"> formulaire de renvoi de lien </a>
