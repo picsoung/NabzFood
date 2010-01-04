2da0cf8b1461ee18a662982f1fc00c377cb8c7a1
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-04 21:34:01 +0100

    valide le compte d'un utilisateur

---
+++ modules/admin/valid_user.php
@@ -0,0 +1,13 @@
+<?php
+    if(!user_admin())
+    {
+	    include PATH_GLOBAL_VIEW.'error_not_admin.php';
+    }
+    else
+    {
+            require_once(PATH_MODEL.'members.php');
+            
+            valid_account($_GET['hash_validation']);
+            header("Location: index.php?module=admin&action=users&id=".$_SESSION['id']); //reload Admin user page
+    }//end of user_admin
+?>
