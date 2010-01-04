bf66929f397b0b1f0a3c91f902b8a10bf409cb2f
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-04 21:34:51 +0100

    Affichage basique de présentation de la banque

---
+++ modules/bank/view/view_bank.php
@@ -0,0 +1,8 @@
+<h2>Nab$z Bank</h2>
+
+<p>Vous pourrez ici recharger votre compte en nab$z, pour cela plusieurs options s'offrent à vous : </p>
+<ul>
+    <li>Cliquer quotidiennement sur la bannière ci dessous et gagner 5 nab$z par clic</li>
+    <li>Appeler le n° audiotel ce qui rechargera votre compte de 100 nab$z</li>
+    <li>Acheter un ticket de la tombola hebdomadaire (10nab$z) pour gagner peut-être le gros lot de 100nab$z</li>
+</ul>
