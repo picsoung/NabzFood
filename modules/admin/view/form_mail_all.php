2ba0069b3aadb4524c587fefb7d9ff774482659d
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-05 22:53:55 +0100

    vue pour envoyer un mail à tous les utilisateurs

---
+++ modules/admin/view/form_mail_all.php
@@ -0,0 +1,5 @@
+<h2>Envoyer un mail à tous les utilisateurs </h2>
+<?php
+    echo $form_mail_all_user;
+?>
+<a href="<?php echo 'index.php?module=admin&action=users&id='.$_SESSION['id'];?>">Revenir à la liste</a>
