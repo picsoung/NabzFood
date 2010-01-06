1d1b9c6edc2a2c53845e43f74015b8cb05705ab2
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-07 00:08:33 +0100

    nouvelle mise en forme du menu

--- global/menu.php
+++ global/menu.php
@@ -1,12 +1,4 @@
 <div id="menu">
-	
-		<h2>Menu</h2>
-		
-		<ul>
-			<li><a href="index.php">Accueil</a></li>
-		</ul>
-		
-		<h3>Espace membre</h3>
 		<?php if (!user_connected()) {?><ul>
 			<li><a href="index.php?module=members&amp;action=signup">Inscription</a></li>
 			<li><a href="index.php?module=members&amp;action=login">Connexion</a></li>
@@ -14,14 +6,15 @@
 		<?php }else{ ?>
 		<p> Bienvenue, <?php echo htmlspecialchars($_SESSION['pseudo']); ?>.</p>
 		<ul>
-			<li><a href="<?php echo 'index.php?module=members&amp;action=profile&id='.$_SESSION['id'];?>">Profil</a></li>
 			<li><a href="<?php echo 'index.php?module=nabz&amp;action=mynabz&id='.$_SESSION['id'];?>">Mon Lapin</a></li>
 			<li><a href="<?php echo 'index.php?module=store&amp;action=buy&id='.$_SESSION['id'];?>">Rabbit Store</a></li>
 			<li><a href="<?php echo 'index.php?module=bank&amp;action=bank&id='.$_SESSION['id'];?>">Banque</a></li>
 			<li><a href="index.php?module=members&amp;action=logout">Déconnexion</a></li>
-			<?php if (user_admin()){?>
-			<li><a href="<?php echo 'index.php?module=admin&amp;action=add_productandcat&id='.$_SESSION['id'];?>">Administration</a></li>
 		</ul>
+			<a href="<?php echo 'index.php?module=members&amp;action=profile&id='.$_SESSION['id'];?>"><img src="<?php echo PATH_IMAGE_RESSOURCE."user.png";?>">Profil</a>
+			<?php if (user_admin()){?>
+				<a href="<?php echo 'index.php?module=admin&amp;action=add_productandcat&id='.$_SESSION['id'];?>"><img src="<?php echo PATH_IMAGE_RESSOURCE."cog.png";?>">Administration</a>
 		<?php }}?>
+		
  
 	</div>
