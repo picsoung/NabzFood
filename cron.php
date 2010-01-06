4713c8c45ec4234bf2922f3d76b0a2fc9fdcf88a
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-06 08:51:56 +0100

    enleve les échos création variable $message appel de la class sendMess.php qui envoi le message au nabz on lui passe nabz_serial, nabz_token et message en paramètre

--- cron.php
+++ cron.php
@@ -4,29 +4,34 @@ include 'global/init.php';
 	require_once(PATH_MODEL.'cron.php');
 	require_once(PATH_MODEL.'nabz.php');
 	$tbx_rabbits_id = list_all_rabbits();
-	print_r($tbx_rabbits_id);
 	foreach ($tbx_rabbits_id as $rabbit)
 	{
 		udapte_angry_skill($rabbit);
 		udapte_health_skill($rabbit);
 		udapte_thirst_skill($rabbit);
-		echo 'rabbit '.$rabbit;
 		
 		$infos_rabbit = read_skill_nabz($rabbit);
-		print_r($infos_rabbit);
+		$sntoken_rabbit = get_serial_and_token($rabbit);
+		print_r($sntoken_rabbit);
+		$nabz_serial = $sntoken_rabbit['rabbit_serial'];
+		$nabz_token = $sntoken_rabbit['rabbit_token'];
 		
-		echo $infos_rabbit['skill_angry'].' '.$infos_rabbit['skill_thirst'].' '.$infos_rabbit['skill_health'];
-		
+		echo 'angry '.$infos_rabbit['skill_angry'];
 		if($infos_rabbit['skill_angry']<50)
 		{
-			echo "J'ai faim";
-		}elseif($infos_rabbit['skill_thirst']<50)
+			$message = "J'ai faim";
+		}
+		if($infos_rabbit['skill_thirst']<50)
 		{
-			echo "J'ai soif";
-			}elseif($infos_rabbit['skill_health']<50)
+			$message .= "J'ai soif";
+		}
+		if($infos_rabbit['skill_health']<50)
 		{
-			echo "Je suis malade";
-			}
+			$message .="Je suis malade";
+		}
+		//$nabz_serial = $tbx_rabbits_id['rabbit_serial'];
+		echo 'nabz serial'.$nabz_serial;
+		include PATH_MODEL.'sendMess.php';
 		
 	}//end foreach
 ?>
