9f8d57c8a1f315cbaa31c060ad3960dfef4bfc0a
Nicolas Grenié <nicolas.grenie@gmail.com>
2010-01-07 00:08:06 +0100

    changement du texte d'accueil

--- global/accueil.php
+++ global/accueil.php
@@ -1,4 +1,8 @@
-<h2>Page d'accueil</h2>
+<h2>Bienvenue sur Nabzfood</h2>
 
-<p>Bienvenue <br />
-(page que l'on trouvera très intéressante)</p>
+<p>Heureux propriétaires d'un Nabaztag ? vous le trouvez un peu terne ? <br />
+Vous trouverez ici de quoi requinquer votre bestiole.</p>
+<p>Nabzfood est une application qui vous permettra de nourrir votre lapin pour qu'il reste en bonne santé. <br />
+Ajoutez une nouvelle dimension dans votre intéraction avec votre compagnon</p>
+
+<p>Nabzfood, le lapin qui a de l'appétit, il vous en remerciera :) </p>
